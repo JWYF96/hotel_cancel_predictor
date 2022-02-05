2a84e11ee126cb8e4a709092e2c644bf8b47df3c
Your Name <you@example.com>
2022-02-05 11:23:48 +0000

    123

--- api_app/main.py
+++ api_app/main.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methos=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def hello_world():
 
     print(request.form)
