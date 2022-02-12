0d17f9a4ad1dcec54281a358f624b65f9717accb
John Wong <john6526@gmail.com>
2022-02-12 18:30:48 +0800

    update2

--- api_app/hotel.py
+++ api_app/hotel.py
@@ -1,25 +1,37 @@
-from flask import Flask, request
+import numpy as np
+import pickle
+from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET', 'POST'])
-def hello_world():
 
-    print(request.form)
+@app.route("/", methods=['GET', 'POST'])
+def machine_learning_model():
 
     param_hotel = request.form.get("hotel_type")
-    param_month = request.form.get("arrival_month")
-    param_num = request.form.get("number_of_people")
-    if param_hotel == 'city':
-        param_hotel = 'City Hotel'
-    elif param_hotel == 'resort':
-        param_hotel = 'Resort Hotel'
+    param_month = request.form.get("number_of_people")
+    param_num = request.form.get("arrival_month")
+
+    if param_hotel is None:
+        return "Please provide hotel type."
+    if param_month is None:
+        return "Please provide arrival month."
+    if param_num is None:
+        return "Please provide number of resident."
+    if not param_month.isdigit():
+        return "Month must be integer."
+    if not param_num.isdigit():
+        return "Number must be integer."
+    if param_hotel == "city":
+        param_hotel = "City Hotel"
+    elif param_hotel == "resort":
+        param_hotel = "Resort Hotel"
+    else:
+        return "Hotel type must be [city, resort]"
 
     param_month = int(param_month)
     param_num = int(param_num)
-
-    import pickle
-    import numpy as np
 
     with open('exported_one_hot.pickle', 'rb') as fp:
         enc = pickle.load(fp)
@@ -28,14 +40,15 @@ def hello_world():
         classifier = pickle.load(fp)
 
     hotel_feature = enc.transform([[param_hotel]]).toarray()
-    month_feature = (int(param_month) >= 6) and (int(param_month) <= 8)
+    month_feature = (param_month >= 6) and (param_month <= 8)
 
-    features = np.hstack([hotel_feature, np.array([[month_feature]]), np.array([[param_num]])])
+    features = np.hstack([
+        hotel_feature,
+        np.array([[month_feature]]),
+        np.array([[param_num]])
+    ])
 
     if classifier.predict(features)[0]:
         return "will not cancel"
     else:
         return "will cancel"
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
